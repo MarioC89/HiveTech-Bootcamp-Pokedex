97abbe12ba4aaef534f9ad1686637338351b175e
MarioC89 <mcrljenkovic@gmail.com>
2022-12-11 20:02:59 +0100

    Login Form

---
+++ src/components/login/LoginForm.js
@@ -0,0 +1,72 @@
+import { Form, Button, Input} from "antd";
+
+const LoginForm = () => {
+    const onFinish = (values) => {
+        console.log(values)
+    }
+    return <div>
+         <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 16,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            autoComplete="off"
+        >
+        <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+            {
+                required: true,
+                message: 'Please input your username!',
+            },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+        <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+            {
+                required: true,
+                message: 'Please input your password!',
+            },
+            ]}
+        >
+            <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+            offset: 8,
+            span: 16,
+            }}
+        >
+        </Form.Item>
+
+        <Form.Item
+            wrapperCol={{
+            offset: 8,
+            span: 16,
+            }}
+        >
+            <Button type="primary" htmlType="submit">
+            Submit
+            </Button>
+        </Form.Item>
+    </Form>
+    </div>
+};
+
+export default LoginForm;
