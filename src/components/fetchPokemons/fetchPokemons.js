a04a97b72fea4619e108d737392f9f08f34673da
MarioC89 <mcrljenkovic@gmail.com>
2022-12-15 00:55:47 +0100

    number of pokemons

--- src/components/fetchPokemons/fetchPokemons.js
+++ src/components/fetchPokemons/fetchPokemons.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import IndividualPokemon from "./IndividualPokemon";
-import styles from "./pokemons.module.css"
+import { Divider } from 'antd';
+
+import styles from "./pokemons.module.css";
 
 const FetchPokemons = () => {
+    const [pokemons, setPokemons] = useState([]);
+    const [pokemonsNumber, setPokemonsNumber] = useState(0);
+
+    const fetchPokemonsHandler = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+            .then((response) => response.json())
+            .then((data) => [
+                setPokemonsNumber(data.count), 
+                setPokemons(data.results),
+            ]);
+    };
+ 
     return (
         <>
-            <button className={styles.button}>Fetch Pokemons!</button>
+            <button className={styles.button} onClick={fetchPokemonsHandler}>
+                Fetch Pokemons!
+            </button>
+            <h2>There are {pokemonsNumber} Pokemons!</h2>
             <ul className={styles.list}>
-                <IndividualPokemon />
+                {pokemons.map((pokemon) => {
+                    return (
+                        <div key={pokemon.url}>
+                            <IndividualPokemon name={pokemon.name} url={pokemon.url}/>
+                            <Divider />
+                        </div>
+                    )
+                })}
             </ul>
         </>
     );
